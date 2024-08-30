24b67d2e345e02968ed7a0606108691c70943723
unknown <letram1132002@gmail.com>
2024-08-30 12:36:38 +0700

    fix UI popular stay

--- src/app/[locale]/stays/page.tsx
+++ src/app/[locale]/stays/page.tsx
@@ -3,13 +3,16 @@
 import Banner from '@/components/Banner/Banner';
 import { NextArrow, PrevArrow } from '@/components/Carousel/ArrowCarousel';
 import { MainClient } from '@/layouts/MainClient';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import './style.scss';
-import { Button, Col, DatePicker, Form, Row, Select } from 'antd';
+import { Button, Col, DatePicker, Form, Input, List, Row, Select, Space } from 'antd';
 import GuestRoomSelector from '@/components/GuestRoomSelect/GuestRoomSelect';
 import { SearchOutlined } from '@ant-design/icons';
 import { useTranslations } from 'next-intl';
+import SectionHead from '@/components/SectionHead/SectionHead';
+import SearchIcon from '@/icons/SearchIcon';
+import CardItem from '@/components/Card/Card';
 
 const carouselBannerSettings = {
   dots: false,
@@ -27,6 +30,87 @@ const carouselBannerSettings = {
 const StayPage = () => {
   const [formRef] = Form.useForm();
   const t = useTranslations('common');
+  const scrollToRef = useRef<HTMLDivElement | null>(null);
+  const [location, setLocation] = useState('');
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const getAddress = [
+    {
+      id: '1',
+      address: '615 Đ. Nguyễn Lương Bằng, Hòa Hiệp Nam, Liên Chiểu, Đà Nẵng, Việt Nam',
+    },
+    {
+      id: '2',
+      address: 'Hà Giang, Việt Nam',
+    },
+  ];
+  const dataMockStays = [
+    {
+      id: '1',
+      staySlug: '/',
+      name: 'Khách sạn Hà Giang',
+      thumbnail: '/images/delivery1.png',
+      amountVN: 3000,
+      amountUS: 0.2,
+      tienVN: 400000,
+      tienUS: 16,
+      rate: 5,
+      place: 'Đà Lạt',
+    },
+    {
+      id: '2',
+      staySlug: '/',
+      name: 'Khách sạn Hà Giang',
+      thumbnail: '/images/delivery1.png',
+      amountVN: 3000,
+      amountUS: 0.2,
+      tienVN: 400000,
+      tienUS: 16,
+      discountPercentage: 10,
+      rate: 5,
+      place: 'Đà Lạt',
+    },
+    {
+      id: '3',
+      staySlug: '/',
+      name: 'Khách sạn Hà Giang',
+      thumbnail: '/images/delivery1.png',
+      amountVN: 3000,
+      amountUS: 0.2,
+      tienVN: 400000,
+      tienUS: 16,
+      discountPercentage: 5,
+      rate: 4,
+      place: 'Đà Lạt',
+    },
+    {
+      id: '4',
+      staySlug: '/',
+      name: 'Khách sạn Hà Giang',
+      thumbnail: '/images/delivery1.png',
+      amountVN: 3000,
+      amountUS: 0.2,
+      tienVN: 400000,
+      tienUS: 16,
+      rate: 5,
+      place: 'Đà Lạt',
+    },
+    {
+      id: '5',
+      staySlug: '/',
+      name: 'Khách sạn Hà Giang',
+      thumbnail: '/images/delivery1.png',
+      amountVN: 3000,
+      amountUS: 0.2,
+      tienVN: 400000,
+      tienUS: 16,
+      rate: 3,
+      place: 'Đà Lạt',
+    },
+  ];
+  useEffect(() => {
+    setLocation(getAddress[0]?.address?.replaceAll(' ', '+'));
+  }, [getAddress]);
 
   const infoBannerStay = [
     {
@@ -110,7 +194,75 @@ const StayPage = () => {
             </Row>
           </Form>
         </div>
-        <div className="wrapper-stays-content"></div>
+        <div className="wrapper-stays-content">
+          <SectionHead
+            miniTitle={t('itemPopular')}
+            description="Product Quality Is Our Priority, And Always Guarantees Halal And Safety Until It Is In Your Hands."
+            title={t('itemStay')}
+            titleIsLeft
+          />
+          <Row gutter={[24, 24]}>
+            <Col xs={24} sm={24} md={12} lg={12} xl={12} className="wrapper-stays-content-left">
+              <div className="wrapper-stays-content-left-search">
+                <Input
+                  type="search"
+                  className="base-input-module"
+                  placeholder={`${t('titleSearchStays')}`}
+                  // onChange={handleInputChange}
+                  // onKeyPress={(e) => handleKeyPress(e)}
+                  // value={searchTerm}
+                  suffix={<Button type="link" icon={<SearchIcon />} />}
+                />
+              </div>
+              <div className="wrapper-stays-content-left-item">
+                <List
+                  grid={{ gutter: 16, column: 2 }}
+                  dataSource={dataMockStays}
+                  renderItem={(item) => (
+                    <List.Item>
+                      <CardItem
+                        key={uuidv4()}
+                        name={item.name}
+                        amountVN={item.amountVN}
+                        amountUS={item.amountUS}
+                        slug={item.staySlug}
+                        thumbnail={item.thumbnail}
+                        tienVN={item.tienVN}
+                        tienUS={item.tienUS}
+                        discountPercentage={item.discountPercentage}
+                        type="STAYS"
+                        rate={item.rate}
+                        place={item.place}
+                      />
+                    </List.Item>
+                  )}
+                />
+              </div>
+            </Col>
+            <Col
+              xs={24}
+              sm={24}
+              md={12}
+              lg={12}
+              xl={12}
+              ref={scrollToRef}
+              id="map-section"
+              style={{ width: '100%' }}
+            >
+              <Space className="map">
+                <iframe
+                  id="map-section"
+                  width="100%"
+                  height={850}
+                  title="Let s travel"
+                  allowFullScreen
+                  referrerPolicy="no-referrer-when-downgrade"
+                  src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyDaOulQACiJzBfqumbsqg_-vKha8fCnL-s&q=${location}`}
+                />
+              </Space>
+            </Col>
+          </Row>
+        </div>
       </div>
     </MainClient>
   );
